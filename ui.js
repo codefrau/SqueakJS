0c2d8477c6078e9d03128db5216c192db7aaa787
Vanessa Freudenberg <vanessa@codefrau.net>
2013-12-15 16:21:31 -0500

    add helper class for dropping an image into Lively

---
+++ ui.js
@@ -0,0 +1,23 @@
+module('lib.squeak.ui').requires("lively.data.FileUpload").toRun(function() {
+
+lively.data.FileUpload.Handler.subclass('lib.squeak.ui.SqueakLoader', {
+    handles: function(file) {
+        return file.type == 'application/squeak-image' || file.name.match(/\.image$/);
+    },
+    getUploadSpec: function(evt, file) {
+        return {readMethod: "asArrayBuffer"}
+    },
+    onLoad: function(evt) {
+        this.openImage(this.file.name, this.file.type, evt.target.result, this.pos);
+    },
+    openImage: function(url, mime, buffer, pos) {
+        var morph = this.findSqueakMorph();
+        if (morph) return morph.loadImageFromBuffer(buffer);
+        alert("Please open a Squeak morph first");
+    },
+    findSqueakMorph: function() {
+        return $world.submorphs.detect(function(morph){return !!morph.loadImageFromBuffer});
+    },
+});
+
+}) // end of module

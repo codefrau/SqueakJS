d3d09f88bde042b3fc270ebf6eaf947ce2a167ed
Vanessa Freudenberg <vanessa@codefrau.net>
2014-07-16 15:58:42 +0200

    update from master: version 0.3.0

--- demo/simple.js
+++ demo/simple.js
@@ -152,15 +152,19 @@ window.onload = function() {
         var image = new Squeak.Image(buffer, name);
         var vm = new Squeak.Interpreter(image, createDisplay());
         var run = function() {
-            vm.interpret(20, function(ms) {
-                if (typeof ms === 'number') { // continue running
-                    loop = window.setTimeout(run, ms);
-                } else { // quit
-                    canvas.style.webkitTransition = "-webkit-transform 0.5s";
-                    canvas.style.webkitTransform = "scale(0)";
-                    window.setTimeout(function(){canvas.style.display = 'none'}, 500);
-                }
-            });
+            try {
+                vm.interpret(20, function(ms) {
+                    if (typeof ms === 'number') { // continue running
+                        loop = window.setTimeout(run, ms);
+                    } else { // quit
+                        canvas.style.webkitTransition = "-webkit-transform 0.5s";
+                        canvas.style.webkitTransform = "scale(0)";
+                        window.setTimeout(function(){canvas.style.display = 'none'}, 500);
+                    }
+                });
+            } catch(error) {
+                alert("Aborting: " + error);
+            }
         };
         run();        
     };

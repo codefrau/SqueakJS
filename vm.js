3c218796fd4d3c9928c04b7a587da0f02f435115
Bert Freudenberg <bert@freudenbergs.de>
2014-07-25 01:55:38 +0200

    release 0.3.3

--- vm.js
+++ vm.js
@@ -26,7 +26,7 @@ Squeak = users.bert.SqueakJS.vm;
 
 Object.extend(Squeak, {
     // system attributes
-    vmVersion: "SqueakJS 0.3.2",
+    vmVersion: "SqueakJS 0.3.3",
     vmBuild: "unknown",                 // replace at runtime by last-modified?
     vmPath: "/SqueakJS/",               // entirely made up
     vmFile: "vm.js",

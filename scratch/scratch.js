e5d81d4859f32adbc50f5a0c3fb07329a13a9940
Vanessa Freudenberg <vanessa@codefrau.net>
2014-09-04 19:51:59 +0200

    update from master: add scratch, update index.html

---
+++ scratch/scratch.js
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2013,2014 Bert Freudenberg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+var fullscreen = navigator.standalone;
+
+window.onload = function() {
+    if (fullscreen) {
+        document.body.style.margin = 0;
+        document.body.style.backgroundColor = 'black';
+        sqHeader.style.display = 'none';
+        sqFooter.style.display = 'none';
+    }
+    var display = createSqueakDisplay(sqCanvas, {fullscreen: fullscreen, header: sqHeader, footer: sqFooter});
+    function loadAndRunImage(url) {
+        //url = "../demo/mini.image"    // for debugging
+        var imageName = Squeak.splitFilePath(url).basename;
+        if (document.location.hostname == "localhost") url = imageName;         // load from local folder while debugging
+        display.showBanner("Downloading " + imageName);
+        display.showProgress(0);
+        var rq = new XMLHttpRequest();
+        rq.open('GET', url);
+        rq.responseType = 'arraybuffer';
+        rq.onprogress = function(e) {
+            if (e.lengthComputable) display.showProgress(e.loaded / e.total);
+        }
+        rq.onload = function(e) {
+            display.showBanner("Initializing, please wait");
+            window.setTimeout(function(){
+                var image = new Squeak.Image(rq.response, imageName);
+                var vm = new Squeak.Interpreter(image, display);
+                display.clear();
+                var run = function() {
+                    try {
+                        vm.interpret(20, function(ms) {
+                            if (typeof ms === 'number') { // continue running
+                                window.setTimeout(run, ms);
+                            } else { // quit
+                                sqCanvas.style.webkitTransition = "-webkit-transform 0.5s";
+                                sqCanvas.style.webkitTransform = "scale(0)";
+                                window.setTimeout(function(){sqCanvas.style.display = 'none'}, 500);
+                            }
+                        });
+                    } catch(error) {
+                        console.error(error);
+                        alert(error);
+                    }
+                };
+                run();
+            }, 0);
+        };
+        rq.send();
+    };
+    loadAndRunImage("http://freudenbergs.de/bert/squeakjs/scratch/Scratch.image");
+};
+
+if (addToHomescreen.isStandalone)
+    fullscreen = true;
+else addToHomescreen({
+   appID: 'squeakjs.scratch.add2home',
+});

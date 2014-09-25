547cdbdafa617c70151958e21599501ee2ccffbe
Vanessa Freudenberg <vanessa@codefrau.net>
2014-09-25 22:38:06 +0200

    Throw error if byte code is not a byte

--- vm.js
+++ vm.js
@@ -1163,155 +1163,156 @@ Object.subclass('Squeak.Interpreter',
             // load receiver variable
             case 0x00: case 0x01: case 0x02: case 0x03: case 0x04: case 0x05: case 0x06: case 0x07: 
             case 0x08: case 0x09: case 0x0A: case 0x0B: case 0x0C: case 0x0D: case 0x0E: case 0x0F: 
-                this.push(this.receiver.pointers[b&0xF]); break;
+                this.push(this.receiver.pointers[b&0xF]); return;
 
             // load temporary variable
             case 0x10: case 0x11: case 0x12: case 0x13: case 0x14: case 0x15: case 0x16: case 0x17: 
             case 0x18: case 0x19: case 0x1A: case 0x1B: case 0x1C: case 0x1D: case 0x1E: case 0x1F: 
-                this.push(this.homeContext.pointers[Squeak.Context_tempFrameStart+(b&0xF)]); break;
+                this.push(this.homeContext.pointers[Squeak.Context_tempFrameStart+(b&0xF)]); return;
 
             // loadLiteral
             case 0x20: case 0x21: case 0x22: case 0x23: case 0x24: case 0x25: case 0x26: case 0x27: 
             case 0x28: case 0x29: case 0x2A: case 0x2B: case 0x2C: case 0x2D: case 0x2E: case 0x2F: 
             case 0x30: case 0x31: case 0x32: case 0x33: case 0x34: case 0x35: case 0x36: case 0x37: 
             case 0x38: case 0x39: case 0x3A: case 0x3B: case 0x3C: case 0x3D: case 0x3E: case 0x3F: 
-                this.push(this.method.methodGetLiteral(b&0x1F)); break;
+                this.push(this.method.methodGetLiteral(b&0x1F)); return;
 
             // loadLiteralIndirect
             case 0x40: case 0x41: case 0x42: case 0x43: case 0x44: case 0x45: case 0x46: case 0x47: 
             case 0x48: case 0x49: case 0x4A: case 0x4B: case 0x4C: case 0x4D: case 0x4E: case 0x4F: 
             case 0x50: case 0x51: case 0x52: case 0x53: case 0x54: case 0x55: case 0x56: case 0x57: 
             case 0x58: case 0x59: case 0x5A: case 0x5B: case 0x5C: case 0x5D: case 0x5E: case 0x5F: 
-                this.push((this.method.methodGetLiteral(b&0x1F)).pointers[Squeak.Assn_value]); break;
+                this.push((this.method.methodGetLiteral(b&0x1F)).pointers[Squeak.Assn_value]); return;
 
             // storeAndPop rcvr, temp
             case 0x60: case 0x61: case 0x62: case 0x63: case 0x64: case 0x65: case 0x66: case 0x67: 
-                this.receiver.pointers[b&7] = this.pop(); break;
+                this.receiver.pointers[b&7] = this.pop(); return;
             case 0x68: case 0x69: case 0x6A: case 0x6B: case 0x6C: case 0x6D: case 0x6E: case 0x6F: 
-                this.homeContext.pointers[Squeak.Context_tempFrameStart+(b&7)] = this.pop(); break;
+                this.homeContext.pointers[Squeak.Context_tempFrameStart+(b&7)] = this.pop(); return;
 
             // Quick push
-            case 0x70: this.push(this.receiver); break;
-            case 0x71: this.push(this.trueObj); break;
-            case 0x72: this.push(this.falseObj); break;
-            case 0x73: this.push(this.nilObj); break;
-            case 0x74: this.push(-1); break;
-            case 0x75: this.push(0); break;
-            case 0x76: this.push(1); break;
-            case 0x77: this.push(2); break;
+            case 0x70: this.push(this.receiver); return;
+            case 0x71: this.push(this.trueObj); return;
+            case 0x72: this.push(this.falseObj); return;
+            case 0x73: this.push(this.nilObj); return;
+            case 0x74: this.push(-1); return;
+            case 0x75: this.push(0); return;
+            case 0x76: this.push(1); return;
+            case 0x77: this.push(2); return;
 
             // Quick return
-            case 0x78: this.doReturn(this.receiver); break;
-            case 0x79: this.doReturn(this.trueObj); break;
-            case 0x7A: this.doReturn(this.falseObj); break;
-            case 0x7B: this.doReturn(this.nilObj); break;
-            case 0x7C: this.doReturn(this.pop()); break;
-            case 0x7D: this.doReturn(this.pop(), this.activeContext.pointers[Squeak.BlockContext_caller]); break; // blockReturn
-            case 0x7E: this.nono(); break;
-            case 0x7F: this.nono(); break;
+            case 0x78: this.doReturn(this.receiver); return;
+            case 0x79: this.doReturn(this.trueObj); return;
+            case 0x7A: this.doReturn(this.falseObj); return;
+            case 0x7B: this.doReturn(this.nilObj); return;
+            case 0x7C: this.doReturn(this.pop()); return;
+            case 0x7D: this.doReturn(this.pop(), this.activeContext.pointers[Squeak.BlockContext_caller]); return; // blockReturn
+            case 0x7E: this.nono(); return;
+            case 0x7F: this.nono(); return;
         } else switch (b) { // Chrome only optimized up to 128 cases
             // Sundry
-            case 0x80: this.extendedPush(this.nextByte()); break;
-            case 0x81: this.extendedStore(this.nextByte()); break;
-            case 0x82: this.extendedStorePop(this.nextByte()); break;
+            case 0x80: this.extendedPush(this.nextByte()); return;
+            case 0x81: this.extendedStore(this.nextByte()); return;
+            case 0x82: this.extendedStorePop(this.nextByte()); return;
             // singleExtendedSend
-            case 0x83: b2 = this.nextByte(); this.send(this.method.methodGetSelector(b2&31), b2>>5, false); break;
-            case 0x84: this.doubleExtendedDoAnything(this.nextByte()); break;
+            case 0x83: b2 = this.nextByte(); this.send(this.method.methodGetSelector(b2&31), b2>>5, false); return;
+            case 0x84: this.doubleExtendedDoAnything(this.nextByte()); return;
             // singleExtendedSendToSuper
-            case 0x85: b2= this.nextByte(); this.send(this.method.methodGetSelector(b2&31), b2>>5, true); break;
+            case 0x85: b2= this.nextByte(); this.send(this.method.methodGetSelector(b2&31), b2>>5, true); return;
             // secondExtendedSend
-            case 0x86: b2= this.nextByte(); this.send(this.method.methodGetSelector(b2&63), b2>>6, false); break;
-            case 0x87: this.pop(); break;	// pop
-            case 0x88: this.push(this.top()); break;	// dup
+            case 0x86: b2= this.nextByte(); this.send(this.method.methodGetSelector(b2&63), b2>>6, false); return;
+            case 0x87: this.pop(); return;	// pop
+            case 0x88: this.push(this.top()); return;	// dup
             // thisContext
-            case 0x89: this.push(this.activeContext); this.reclaimableContextCount = 0; break;
+            case 0x89: this.push(this.activeContext); this.reclaimableContextCount = 0; return;
 
             // Closures
             case 0x8A: this.pushNewArray(this.nextByte());   // create new temp vector
-                break;
-            case 0x8B: this.nono(); break;
+                return;
+            case 0x8B: this.nono(); return;
             case 0x8C: b2 = this.nextByte(); // remote push from temp vector
                 this.push(this.homeContext.pointers[Squeak.Context_tempFrameStart+this.nextByte()].pointers[b2]);
-                break;
+                return;
             case 0x8D: b2 = this.nextByte(); // remote store into temp vector
                 this.homeContext.pointers[Squeak.Context_tempFrameStart+this.nextByte()].pointers[b2] = this.top();
-                break;
+                return;
             case 0x8E: b2 = this.nextByte(); // remote store and pop into temp vector
                 this.homeContext.pointers[Squeak.Context_tempFrameStart+this.nextByte()].pointers[b2] = this.pop();
-                break;
-            case 0x8F: this.pushClosureCopy(); break;
+                return;
+            case 0x8F: this.pushClosureCopy(); return;
 
             // Short jmp
             case 0x90: case 0x91: case 0x92: case 0x93: case 0x94: case 0x95: case 0x96: case 0x97: 
-                this.pc += (b&7)+1; break;
+                this.pc += (b&7)+1; return;
             // Short conditional jump on false
             case 0x98: case 0x99: case 0x9A: case 0x9B: case 0x9C: case 0x9D: case 0x9E: case 0x9F: 
-                this.jumpIfFalse((b&7)+1); break;
+                this.jumpIfFalse((b&7)+1); return;
             // Long jump, forward and back
             case 0xA0: case 0xA1: case 0xA2: case 0xA3: case 0xA4: case 0xA5: case 0xA6: case 0xA7: 
                 b2 = this.nextByte();
                 this.pc += (((b&7)-4)*256 + b2);
                 if ((b&7)<4)        // check for process switch on backward jumps (loops)
                     if (this.interruptCheckCounter-- <= 0) this.checkForInterrupts();
-                break;
+                return;
             // Long conditional jump on true
             case 0xA8: case 0xA9: case 0xAA: case 0xAB:
-                this.jumpIfTrue((b&3)*256 + this.nextByte()); break;
+                this.jumpIfTrue((b&3)*256 + this.nextByte()); return;
             // Long conditional jump on false
             case 0xAC: case 0xAD: case 0xAE: case 0xAF: 
-                this.jumpIfFalse((b&3)*256 + this.nextByte()); break;
+                this.jumpIfFalse((b&3)*256 + this.nextByte()); return;
 
             // Arithmetic Ops... + - < > <= >= = ~=    * /  @ lshift: lxor: land: lor:
             case 0xB0: this.success = true; this.resultIsFloat = false;
-                if(!this.pop2AndPushNumResult(this.stackIntOrFloat(1) + this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); break;	// PLUS +
+                if(!this.pop2AndPushNumResult(this.stackIntOrFloat(1) + this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); return;	// PLUS +
             case 0xB1: this.success = true; this.resultIsFloat = false;
-                if(!this.pop2AndPushNumResult(this.stackIntOrFloat(1) - this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); break;	// MINUS -
+                if(!this.pop2AndPushNumResult(this.stackIntOrFloat(1) - this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); return;	// MINUS -
             case 0xB2: this.success = true;
-                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) < this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); break;  // LESS <
+                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) < this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); return;  // LESS <
             case 0xB3: this.success = true;
-                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) > this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); break;  // GRTR >
+                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) > this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); return;  // GRTR >
             case 0xB4: this.success = true;
-                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) <= this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); break;  // LEQ <=
+                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) <= this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); return;  // LEQ <=
             case 0xB5: this.success = true;
-                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) >= this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); break;  // GEQ >=
+                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) >= this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); return;  // GEQ >=
             case 0xB6: this.success = true;
-                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) === this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); break;  // EQU =
+                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) === this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); return;  // EQU =
             case 0xB7: this.success = true;
-                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) !== this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); break;  // NEQ ~=
+                if(!this.pop2AndPushBoolResult(this.stackIntOrFloat(1) !== this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); return;  // NEQ ~=
             case 0xB8: this.success = true; this.resultIsFloat = false;
-                if(!this.pop2AndPushNumResult(this.stackIntOrFloat(1) * this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); break;  // TIMES *
+                if(!this.pop2AndPushNumResult(this.stackIntOrFloat(1) * this.stackIntOrFloat(0))) this.sendSpecial(b&0xF); return;  // TIMES *
             case 0xB9: this.success = true;
-                if(!this.pop2AndPushIntResult(this.quickDivide(this.stackInteger(1),this.stackInteger(0)))) this.sendSpecial(b&0xF); break;  // Divide /
+                if(!this.pop2AndPushIntResult(this.quickDivide(this.stackInteger(1),this.stackInteger(0)))) this.sendSpecial(b&0xF); return;  // Divide /
             case 0xBA: this.success = true;
-                if(!this.pop2AndPushIntResult(this.mod(this.stackInteger(1),this.stackInteger(0)))) this.sendSpecial(b&0xF); break;  // MOD \
+                if(!this.pop2AndPushIntResult(this.mod(this.stackInteger(1),this.stackInteger(0)))) this.sendSpecial(b&0xF); return;  // MOD \
             case 0xBB: this.success = true;
-                if(!this.primHandler.primitiveMakePoint(1)) this.sendSpecial(b&0xF); break;  // MakePt int@int
+                if(!this.primHandler.primitiveMakePoint(1)) this.sendSpecial(b&0xF); return;  // MakePt int@int
             case 0xBC: this.success = true;
-                if(!this.pop2AndPushIntResult(this.safeShift(this.stackInteger(1),this.stackInteger(0)))) this.sendSpecial(b&0xF); break; // bitShift:
+                if(!this.pop2AndPushIntResult(this.safeShift(this.stackInteger(1),this.stackInteger(0)))) this.sendSpecial(b&0xF); return; // bitShift:
             case 0xBD: this.success = true;
-                if(!this.pop2AndPushIntResult(this.div(this.stackInteger(1),this.stackInteger(0)))) this.sendSpecial(b&0xF); break;  // Divide //
+                if(!this.pop2AndPushIntResult(this.div(this.stackInteger(1),this.stackInteger(0)))) this.sendSpecial(b&0xF); return;  // Divide //
             case 0xBE: this.success = true;
-                if(!this.pop2AndPushIntResult(this.stackInteger(1) & this.stackInteger(0))) this.sendSpecial(b&0xF); break; // bitAnd:
+                if(!this.pop2AndPushIntResult(this.stackInteger(1) & this.stackInteger(0))) this.sendSpecial(b&0xF); return; // bitAnd:
             case 0xBF: this.success = true;
-                if(!this.pop2AndPushIntResult(this.stackInteger(1) | this.stackInteger(0))) this.sendSpecial(b&0xF); break; // bitOr:
+                if(!this.pop2AndPushIntResult(this.stackInteger(1) | this.stackInteger(0))) this.sendSpecial(b&0xF); return; // bitOr:
 
             // at:, at:put:, size, next, nextPut:, ...
             case 0xC0: case 0xC1: case 0xC2: case 0xC3: case 0xC4: case 0xC5: case 0xC6: case 0xC7: 
             case 0xC8: case 0xC9: case 0xCA: case 0xCB: case 0xCC: case 0xCD: case 0xCE: case 0xCF: 
                 if (!this.primHandler.quickSendOther(this.receiver, b&0xF))
-                    this.sendSpecial((b&0xF)+16); break;
+                    this.sendSpecial((b&0xF)+16); return;
 
             // Send Literal Selector with 0, 1, and 2 args
             case 0xD0: case 0xD1: case 0xD2: case 0xD3: case 0xD4: case 0xD5: case 0xD6: case 0xD7: 
             case 0xD8: case 0xD9: case 0xDA: case 0xDB: case 0xDC: case 0xDD: case 0xDE: case 0xDF: 
-                this.send(this.method.methodGetSelector(b&0xF), 0, false); break;
+                this.send(this.method.methodGetSelector(b&0xF), 0, false); return;
             case 0xE0: case 0xE1: case 0xE2: case 0xE3: case 0xE4: case 0xE5: case 0xE6: case 0xE7: 
             case 0xE8: case 0xE9: case 0xEA: case 0xEB: case 0xEC: case 0xED: case 0xEE: case 0xEF: 
-                this.send(this.method.methodGetSelector(b&0xF), 1, false); break;
+                this.send(this.method.methodGetSelector(b&0xF), 1, false); return;
             case 0xF0: case 0xF1: case 0xF2: case 0xF3: case 0xF4: case 0xF5: case 0xF6: case 0xF7: 
             case 0xF8: case 0xF9: case 0xFA: case 0xFB: case 0xFC: case 0xFD: case 0xFE: case 0xFF:
-                this.send(this.method.methodGetSelector(b&0xF), 2, false); break;
+                this.send(this.method.methodGetSelector(b&0xF), 2, false); return;
         }
+        throw Error("not a bytecode: " + b);
     },
     interpret: function(forMilliseconds, thenDo) {
         // run for a couple milliseconds (but only until idle or break)

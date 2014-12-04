d0ac9b683cfe06e5f1c786ccc373d0028db2e8bd
Vanessa Freudenberg <vanessa@codefrau.net>
2014-12-04 22:19:52 +0100

    release 0.7

--- vm.js
+++ vm.js
@@ -27,7 +27,7 @@ window.Squeak = users.bert.SqueakJS.vm;
 Object.extend(Squeak,
 "version", {
     // system attributes
-    vmVersion: "SqueakJS 0.6.8",
+    vmVersion: "SqueakJS 0.7",
     vmBuild: "unknown",                 // replace at runtime by last-modified?
     vmPath: "/",
     vmFile: "vm.js",

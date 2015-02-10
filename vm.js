03cf835f631ca0f88ecd319d695e19db606ed108
Nils Kenneweg <nk@ovt.me>
2015-02-10 13:26:16 +0100

    use the right source for argCount to fix messageNotUnderstood and prevent caching if argCount changed

--- vm.js
+++ vm.js
@@ -2378,13 +2378,14 @@ Object.subclass('Squeak.Interpreter',
             entry.method.lastFlush = entry.method.lastFlush || 1;
             selector.lastFlush = selector.lastFlush || 1;
 
-            this.method.ic[this.pc] = {
-                added: new Date().getTime(),
-                method: entry.method,
-                primIndex: entry.primIndex,
-                mClass: entry.mClass,
-                lkupClass: entry.lkupClass
-            }
+            if (entry.argCount === argCount)
+                this.method.ic[this.pc] = {
+                    added: new Date().getTime(),
+                    method: entry.method,
+                    primIndex: entry.primIndex,
+                    mClass: entry.mClass,
+                    lkupClass: entry.lkupClass
+                }
         } else if (ic.lkupClass !== lookupClass) {
             entry = this.findSelectorInClass(selector, argCount, lookupClass);            
         } else {
@@ -2397,7 +2398,7 @@ Object.subclass('Squeak.Interpreter',
             this.verifyAtClass = lookupClass;
         }
 
-        this.executeNewMethod(newRcvr, entry.method, argCount, entry.primIndex, entry.mClass, selector);
+        this.executeNewMethod(newRcvr, entry.method, entry.argCount || argCount, entry.primIndex, entry.mClass, selector);
     },
     sendAsPrimitiveFailure: function(rcvr, method, argCount) {
         this.executeNewMethod(rcvr, method, argCount, 0);

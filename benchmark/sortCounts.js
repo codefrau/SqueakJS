1fcea4e48caf1f7443fbfd321ee8e210024f1fc3
Bert Freudenberg <bert@freudenbergs.de>
2015-04-15 16:57:49 -0700

    Remove leftover files

--- benchmark/sortCounts.js
+++
@@ -1,5 +0,0 @@
-
-var peepholes = Object.keys(peepholesCounter)
-peepholes.map(function (name) { return { name: name, count: peepholesCounter[name] }; })
-var data = peepholes.map(function (name) { return { name: name, count: peepholesCounter[name] }; })
-data.sort(function (a, b) { return b.count - a.count; })

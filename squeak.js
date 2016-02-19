8032425e6ed464b0128128fae30ae220074ec790
Bert Freudenberg <bert@freudenbergs.de>
2016-02-19 15:21:55 -0800

    Update copyright

--- squeak.js
+++ squeak.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013,2014 Bert Freudenberg
+ * Copyright (c) 2013-2016 Bert Freudenberg
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

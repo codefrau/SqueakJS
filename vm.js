codefrau/SqueakJS

commit db48e7ab728b87827e011b1108515d1927a079be

Class name: look in slots 6 and 7

vm.js
@@ -2202,14 +2202,18 @@ Object.subclass('Squeak.Object',
     },
     className: function() {
         if (!this.pointers) return "_NOTACLASS_";
-        var name = this.pointers[Squeak.Class_name];
-        if (name && name.bytes) return name.bytesAsString();
+        for (var nameIdx = 6; nameIdx <= 7; nameIdx++) {
+            var name = this.pointers[nameIdx];
+            if (name && name.bytes) return name.bytesAsString();
+        }
         // must be meta class
         for (var clsIndex = 5; clsIndex <= 6; clsIndex++) {
             var cls = this.pointers[clsIndex];
             if (cls && cls.pointers) {
-                name = cls.pointers[Squeak.Class_name];
-                if (name && name.bytes) return name.bytesAsString() + " class";
+                for (var nameIdx = 6; nameIdx <= 7; nameIdx++) {
+                    var name = cls.pointers[nameIdx];
+                    if (name && name.bytes) return name.bytesAsString();
+                }
             }
         }
         return "_SOMECLASS_";

commit bbb8098ab8d107cd869d6fd8738db129625bde45

Unbreak JSBridge demo (oops)
Meta class name lookup was broken since
db48e7ab728b87827e011b1108515d1927a079be

vm.js
@@ -2213,7 +2213,7 @@ Object.subclass('Squeak.Object',
             if (cls && cls.pointers) {
                 for (var nameIdx = 6; nameIdx <= 7; nameIdx++) {
                     var name = cls.pointers[nameIdx];
-                    if (name && name.bytes) return name.bytesAsString();
+                    if (name && name.bytes) return name.bytesAsString() + " class";
                 }
             }
         }
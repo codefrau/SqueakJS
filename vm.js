3d8fcdf8942794d2ee839f9b3b860bc05736847d
Bert Freudenberg <bert@freudenbergs.de>
2016-11-08 16:37:08 -0300

    release 0.9.5

--- vm.js
+++ vm.js
@@ -39,7 +39,7 @@ try {
 Object.extend(Squeak,
 "version", {
     // system attributes
-    vmVersion: "SqueakJS 0.9.4",
+    vmVersion: "SqueakJS 0.9.5",
     vmBuild: "unknown",                 // replace at runtime by last-modified?
     vmPath: "/",
     vmFile: "vm.js",

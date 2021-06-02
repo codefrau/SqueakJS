b4db7f7f6b4834ee51795992f0976b152f93029f
Vanessa Freudenberg <vanessa@codefrau.net>
2021-06-02 10:08:09 -0700

    forgot to bump vmVersion

--- dist/squeak_bundle.js
+++ dist/squeak_bundle.js
@@ -113,8 +113,8 @@
     Object.extend(Squeak,
     "version", {
         // system attributes
-        vmVersion: "SqueakJS 1.0.3",
-        vmDate: "2021-03-21",               // Maybe replace at build time?
+        vmVersion: "SqueakJS 1.0.4",
+        vmDate: "2021-05-31",               // Maybe replace at build time?
         vmBuild: "unknown",                 // or replace at runtime by last-modified?
         vmPath: "unknown",                  // Replace at runtime
         vmFile: "vm.js",

2ee8faab6826868069b38b4be49c65266329cf39
Vanessa Freudenberg <vanessa@codefrau.net>
2023-12-08 21:21:25 -0800

    Add FFI errors

--- vm.plugins.ffi.js
+++ vm.plugins.ffi.js
@@ -30,24 +30,34 @@ Object.extend(Squeak,
     ExtLibFunc_name: 3,
     ExtLibFunc_module: 4,
     ExtLibFunc_errorCodeName: 5,
+},
+"FFI error codes", {
+    FFINoCalloutAvailable: -1, // No callout mechanism available
+    FFIErrorGenericError: 0, // generic error
+    FFIErrorNotFunction: 1, // primitive invoked without ExternalFunction
+    FFIErrorBadArgs: 2, // bad arguments to primitive call
+    FFIErrorBadArg: 3, // generic bad argument
+    FFIErrorIntAsPointer: 4, // int passed as pointer
+    FFIErrorBadAtomicType: 5, // bad atomic type (e.g., unknown)
+    FFIErrorCoercionFailed: 6, // argument coercion failed
+    FFIErrorWrongType: 7, // Type check for non-atomic types failed
+    FFIErrorStructSize: 8, // struct size wrong or too large
+    FFIErrorCallType: 9, // unsupported calling convention
+    FFIErrorBadReturn: 10, // cannot return the given type
+    FFIErrorBadAddress: 11, // bad function address
+    FFIErrorNoModule: 12, // no module given but required for finding address
+    FFIErrorAddressNotFound: 13, // function address not found
+    FFIErrorAttemptToPassVoid: 14, // attempt to pass 'void' parameter
+    FFIErrorModuleNotFound: 15, // module not found
+    FFIErrorBadExternalLibrary: 16, // external library invalid
+    FFIErrorBadExternalFunction: 17, // external function invalid
+    FFIErrorInvalidPointer: 18, // ExternalAddress points to ST memory (don't you dare to do this!)
+    FFIErrorCallFrameTooBig: 19, // Stack frame required more than 16k bytes to pass arguments.
 });
 
 Object.extend(Squeak.Primitives.prototype,
 'FFI', {
-    primitiveCalloutToFFI: function(argCount, method) {
-        var extLibFunc = method.pointers[1];
-        if (!this.isKindOf(extLibFunc, Squeak.splOb_ClassExternalFunction)) return false;
-        var args = [];
-        for (var i = argCount - 1; i >= 0; i--)
-            args.push(this.vm.stackValue(i));
-        return this.calloutToFFI(argCount, extLibFunc, args);
-    },
-    ffi_primitiveCalloutWithArgs: function(argCount) {
-        var extLibFunc = this.stackNonInteger(1),
-            argsObj = this.stackNonInteger(0);
-        if (!this.isKindOf(extLibFunc, Squeak.splOb_ClassExternalFunction)) return false;
-        return this.calloutToFFI(argCount, extLibFunc, argsObj.pointers);
-    },
+    ffi_lastError: 0,
     calloutToFFI: function(argCount, extLibFunc, stArgs) {
         var moduleName = extLibFunc.pointers[Squeak.ExtLibFunc_module].bytesAsString();
         var funcName = extLibFunc.pointers[Squeak.ExtLibFunc_name].bytesAsString();
@@ -63,5 +73,22 @@ Object.extend(Squeak.Primitives.prototype,
         var jsResult = module[funcName].apply(this, jsArgs);
         var stResult = this.makeStObject(jsResult);
         return this.popNandPushIfOK(argCount + 1, stResult);
+    },
+    primitiveCalloutToFFI: function(argCount, method) {
+        var extLibFunc = method.pointers[1];
+        if (!this.isKindOf(extLibFunc, Squeak.splOb_ClassExternalFunction)) return false;
+        var args = [];
+        for (var i = argCount - 1; i >= 0; i--)
+            args.push(this.vm.stackValue(i));
+        return this.calloutToFFI(argCount, extLibFunc, args);
+    },
+    ffi_primitiveCalloutWithArgs: function(argCount) {
+        var extLibFunc = this.stackNonInteger(1),
+            argsObj = this.stackNonInteger(0);
+        if (!this.isKindOf(extLibFunc, Squeak.splOb_ClassExternalFunction)) return false;
+        return this.calloutToFFI(argCount, extLibFunc, argsObj.pointers);
+    },
+    ffi_primitiveFFIGetLastError: function(argCount) {
+        return this.popNandPushIfOK(argCount + 1, this.ffi_lastError);
     },
 });

9fcdc272a276a5af8130854494c75d51ddc43bd1
Vanessa Freudenberg <vanessa@codefrau.net>
2024-02-18 21:32:54 -0800

    Fix keyboard charset

--- squeak.js
+++ squeak.js
@@ -177,7 +177,7 @@ function setupSwapButtons(options) {
 function recordModifiers(evt, display) {
     var shiftPressed = evt.shiftKey,
         ctrlPressed = evt.ctrlKey && !evt.altKey,
-        cmdPressed = evt.metaKey || (evt.altKey && !evt.ctrlKey),
+        cmdPressed = display.isMac ? evt.metaKey : evt.altKey && !evt.ctrlKey,
         modifiers =
             (shiftPressed ? Squeak.Keyboard_Shift : 0) +
             (ctrlPressed ? Squeak.Keyboard_Ctrl : 0) +
@@ -246,29 +246,53 @@ function recordMouseEvent(what, evt, canvas, display, options) {
     }
 }
 
-function recordKeyboardEvent(key, timestamp, display) {
+// Squeak traditional keycodes are MacRoman
+var MacRomanToUnicode = [
+    0x00C4, 0x00C5, 0x00C7, 0x00C9, 0x00D1, 0x00D6, 0x00DC, 0x00E1,
+    0x00E0, 0x00E2, 0x00E4, 0x00E3, 0x00E5, 0x00E7, 0x00E9, 0x00E8,
+    0x00EA, 0x00EB, 0x00ED, 0x00EC, 0x00EE, 0x00EF, 0x00F1, 0x00F3,
+    0x00F2, 0x00F4, 0x00F6, 0x00F5, 0x00FA, 0x00F9, 0x00FB, 0x00FC,
+    0x2020, 0x00B0, 0x00A2, 0x00A3, 0x00A7, 0x2022, 0x00B6, 0x00DF,
+    0x00AE, 0x00A9, 0x2122, 0x00B4, 0x00A8, 0x2260, 0x00C6, 0x00D8,
+    0x221E, 0x00B1, 0x2264, 0x2265, 0x00A5, 0x00B5, 0x2202, 0x2211,
+    0x220F, 0x03C0, 0x222B, 0x00AA, 0x00BA, 0x03A9, 0x00E6, 0x00F8,
+    0x00BF, 0x00A1, 0x00AC, 0x221A, 0x0192, 0x2248, 0x2206, 0x00AB,
+    0x00BB, 0x2026, 0x00A0, 0x00C0, 0x00C3, 0x00D5, 0x0152, 0x0153,
+    0x2013, 0x2014, 0x201C, 0x201D, 0x2018, 0x2019, 0x00F7, 0x25CA,
+    0x00FF, 0x0178, 0x2044, 0x20AC, 0x2039, 0x203A, 0xFB01, 0xFB02,
+    0x2021, 0x00B7, 0x201A, 0x201E, 0x2030, 0x00C2, 0x00CA, 0x00C1,
+    0x00CB, 0x00C8, 0x00CD, 0x00CE, 0x00CF, 0x00CC, 0x00D3, 0x00D4,
+    0xF8FF, 0x00D2, 0x00DA, 0x00DB, 0x00D9, 0x0131, 0x02C6, 0x02DC,
+    0x00AF, 0x02D8, 0x02D9, 0x02DA, 0x00B8, 0x02DD, 0x02DB, 0x02C7,
+];
+var UnicodeToMacRoman = {};
+for (var i = 0; i < MacRomanToUnicode.length; i++)
+    UnicodeToMacRoman[MacRomanToUnicode[i]] = i + 128;
+
+function recordKeyboardEvent(unicode, timestamp, display) {
     if (!display.vm) return;
-    var code = (display.buttons >> 3) << 8 | key;
+    var macCode = UnicodeToMacRoman[unicode] || unicode;
+    var modifiersAndKey = (display.buttons >> 3) << 8 | macCode;
     if (display.eventQueue) {
         display.eventQueue.push([
             Squeak.EventTypeKeyboard,
             timestamp,  // converted to Squeak time in makeSqueakEvent()
-            key, // MacRoman
+            macCode, // MacRoman
             Squeak.EventKeyChar,
             display.buttons >> 3,
-            key,  // Unicode
+            unicode,  // Unicode
         ]);
         if (display.signalInputEvent)
             display.signalInputEvent();
         // There are some old images that use both event-based
         // and polling primitives. To make those work, keep the
         // last key event
-        display.keys[0] = code;
-    } else if (code === display.vm.interruptKeycode) {
+        display.keys[0] = modifiersAndKey;
+    } else if (modifiersAndKey === display.vm.interruptKeycode) {
         display.vm.interruptPending = true;
     } else {
         // no event queue, queue keys the old-fashioned way
-        display.keys.push(code);
+        display.keys.push(modifiersAndKey);
     }
     display.idle = 0;
     if (display.runNow) display.runNow(); // don't wait for timeout to run
@@ -686,10 +710,11 @@ function createSqueakDisplay(canvas, options) {
         canvas.style.cursor = "none";
     }
     // keyboard stuff
+    display.isMac = navigator.userAgent.includes("Mac");
     document.onkeypress = function(evt) {
         if (!display.vm) return true;
         // check for ctrl-x/c/v/r
-        if (/[CXVR]/.test(String.fromCharCode(evt.charCode + 64)))
+        if (!display.isMac && /[CXVR]/.test(String.fromCharCode(evt.charCode + 64)))
             return true;  // let browser handle cut/copy/paste/reload
         recordModifiers(evt, display);
         recordKeyboardEvent(evt.charCode, evt.timeStamp, display);
@@ -698,6 +723,7 @@ function createSqueakDisplay(canvas, options) {
     document.onkeydown = function(evt) {
         checkFullscreen();
         if (!display.vm) return true;
+        if (evt.key === "Dead") return true;  // let browser handle dead keys
         recordModifiers(evt, display);
         var squeakCode = ({
             8: 8,   // Backspace
@@ -720,12 +746,12 @@ function createSqueakDisplay(canvas, options) {
             recordKeyboardEvent(squeakCode, evt.timeStamp, display);
             return evt.preventDefault();
         }
-        if ((evt.metaKey || (evt.altKey && !evt.ctrlKey))) {
+        if (display.buttons & Squeak.Keyboard_Cmd) {
             var key = evt.key; // only supported in FireFox, others have keyIdentifier
             if (!key && evt.keyIdentifier && evt.keyIdentifier.slice(0,2) == 'U+')
                 key = String.fromCharCode(parseInt(evt.keyIdentifier.slice(2), 16));
             if (key && key.length == 1) {
-                if (/[CXVR]/i.test(key))
+                if (display.isMac && /[CXVR]/i.test(key))
                     return true;  // let browser handle cut/copy/paste/reload
                 var code = key.charCodeAt(0);
                 if (/[A-Z]/.test(key) && !evt.shiftKey) code += 32;  // make lower-case

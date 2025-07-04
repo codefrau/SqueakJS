db6538be9d27f65027c357f65af65ee4ec2b6859
Vanessa Freudenberg <vanessa@codefrau.net>
2025-07-04 11:44:16 +0200

    update from main

--- demo/simple.js
+++ demo/simple.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020 Vanessa Freudenberg
+ * Copyright (c) 2013-2025 Vanessa Freudenberg
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
